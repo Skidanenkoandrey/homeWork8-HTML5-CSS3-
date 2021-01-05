9ee24925b7067d3ad338e1f6c9587087f3b35a61
Skydanenko Andrii <skidanenkoandrey@gmail.com>
2021-01-05 12:02:58 +0200

    Added html structure to the Courses section and stylized some styles

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -41,6 +41,7 @@ module.exports = {
         }, {
             test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
             loader: 'file-loader',
+            exclude: [/img/],
             options: {
                 name: '[name].[ext]',
                 outputPath: `/${PATHS.assets}/fonts`
